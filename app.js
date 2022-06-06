5d56094d6498d28de5c60b15495d4615823b3203
olmpc <k_lavatai@yahoo.com>
2022-06-06 09:27:19 -0700

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://scxn:1269lynnway@cluster0.jrgou.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://scxn:1269lynnway@cluster0.jrgou.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
